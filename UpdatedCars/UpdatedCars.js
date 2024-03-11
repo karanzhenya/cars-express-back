c108ca6463fc56a207a62dedf2b9e38aafc75873
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2024-03-12 00:32:30 +0300

    add car id in updated car schema

--- UpdatedCars/UpdatedCars.js
+++ UpdatedCars/UpdatedCars.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const UpdatedCars = new mongoose.Schema({
     modelType: {type: String, require: true},
     info: {type: String, require: true},
-    date: {type: String, require: true}
+    date: {type: String, require: true},
+    _car_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Car'}
 })
 
 export default mongoose.model('UpdatedCars', UpdatedCars)
